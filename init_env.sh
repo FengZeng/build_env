84c7c891135967f3cd3b072aa02aea82a6d9ac07
ZengFeng <jerozeng@163.com>
2021-09-17 12:29:25 +0800

    if /usr/local already exist, delete it first

--- init_env.sh
+++ init_env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+DST=`cygpath -w /usr/local` 
 if [ ! -d /usr/local ]; then
-    CurPath=`pwd`
-    DST=`cygpath -w /usr/local` 
+    CurPath=`pwd` 
     SRC=`cygpath -w $CurPath/local`
     echo $DST
     echo $SRC
     cmd "/C .\local\link.bat "$DST" "$SRC""
+else
+    echo "$DST already exist, delete it first!"
 fi
